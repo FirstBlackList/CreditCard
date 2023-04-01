dd89e558f32b358a2cb74d8d813c37cd2cc429e6
BlackList <azokblack@gmail.com>
2023-04-01 22:01:45 +0400

    Assembling_2

--- src/test/java/ru/netology/creditcard/PreparingForTests.java
+++ src/test/java/ru/netology/creditcard/PreparingForTests.java
@@ -1,5 +1,6 @@
 package ru.netology.creditcard;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +13,8 @@ public class PreparingForTests {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
 
     @BeforeEach
